6b41033ebdad3e1cd70deab83983fada537722b6
Alexey Biriuchenko <gotastycake@gmail.com>
2021-10-17 15:36:45 +0300

    fix players movement

--- Assets/Controllers/Cell.cs
+++ Assets/Controllers/Cell.cs
@@ -45,69 +45,39 @@ public class Cell : MonoBehaviour
         if (Game.gameArray[x, y] != Constants.FIELD_ID) { return false; }
         int playerId = Game.currentPlayer.GetComponent<Player>().Id;
         int oppositePlayerId = Game.oppositePlayer.GetComponent<Player>().Id;
+        
         // if there are no walls on n, e, s, w
         if (Game.gameArray[l(x - 2), y] == playerId && Game.gameArray[l(x - 1), y] != Constants.WALL_ID) { return true; }
         if (Game.gameArray[l(x + 2), y] == playerId && Game.gameArray[l(x + 1), y] != Constants.WALL_ID) { return true; }
         if (Game.gameArray[x, l(y - 2)] == playerId && Game.gameArray[x, l(y - 1)] != Constants.WALL_ID) { return true; }
         if (Game.gameArray[x, l(y + 2)] == playerId && Game.gameArray[x, l(y + 1)] != Constants.WALL_ID) { return true; }
 
+        
+
         // if there is a player and no walls
-        if (Game.gameArray[l(x - 2), y] == oppositePlayerId && 
-        (Game.gameArray[l(x - 4), y] == playerId || Game.gameArray[l(x - 2), l(y - 2)] == playerId || Game.gameArray[l(x - 2), l(y + 2)] == playerId)) {
-            if (Game.gameArray[l(x - 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x - 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x - 3), y] != Constants.WALL_ID && Game.gameArray[l(x - 4), y] == Constants.FIELD_ID) { return true; }
+        if (Game.gameArray[l(x - 2), y] == oppositePlayerId && Game.gameArray[l(x - 1), y] != Constants.WALL_ID) {
+            if (Game.gameArray[l(x - 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x - 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x - 3), y] != Constants.WALL_ID && Game.gameArray[l(x - 4), y] == playerId) { return true; }
         }
 
-        if (Game.gameArray[l(x + 2), y] == oppositePlayerId && 
-        (Game.gameArray[l(x + 4), y] == playerId || Game.gameArray[l(x + 2), l(y - 2)] == playerId || Game.gameArray[l(x + 2), l(y + 2)] == playerId)) {
-            if (Game.gameArray[l(x + 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x + 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x + 3), y] != Constants.WALL_ID && Game.gameArray[l(x + 4), y] == Constants.FIELD_ID) { return true; }
+        if (Game.gameArray[l(x + 2), y] == oppositePlayerId && Game.gameArray[l(x + 1), y] != Constants.WALL_ID) {
+            if (Game.gameArray[l(x + 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x + 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x + 3), y] != Constants.WALL_ID && Game.gameArray[l(x + 4), y] == playerId) { return true; }
         }
 
-        if (Game.gameArray[x, l(y - 2)] == oppositePlayerId &&
-        (Game.gameArray[x, l(y - 4)] == playerId || Game.gameArray[l(x - 2), l(y - 2)] == playerId || Game.gameArray[l(x + 2), l(y - 2)] == playerId)) {
-            if (Game.gameArray[l(x - 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x + 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[x, l(y - 3)] != Constants.WALL_ID && Game.gameArray[x, l(y - 4)] == Constants.FIELD_ID) { return true; }
+        if (Game.gameArray[x, l(y - 2)] == oppositePlayerId && Game.gameArray[x, l(y - 1)] != Constants.WALL_ID) {
+            if (Game.gameArray[l(x - 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x + 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == playerId) { return true; }
+            if (Game.gameArray[x, l(y - 3)] != Constants.WALL_ID && Game.gameArray[x, l(y - 4)] == playerId) { return true; }
         }
 
-        if (Game.gameArray[x, l(y + 2)] == oppositePlayerId &&
-        (Game.gameArray[x, l(y + 4)] == playerId || Game.gameArray[l(x - 2), l(y + 2)] == playerId || Game.gameArray[l(x + 2), l(y + 2)] == playerId)) {
-            if (Game.gameArray[l(x - 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[l(x + 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-            if (Game.gameArray[x, l(y + 3)] != Constants.WALL_ID && Game.gameArray[x, l(y + 4)] == Constants.FIELD_ID) { return true; }
+        if (Game.gameArray[x, l(y + 2)] == oppositePlayerId && Game.gameArray[x, l(y + 1)] != Constants.WALL_ID) {
+            if (Game.gameArray[l(x - 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == playerId) { return true; }
+            if (Game.gameArray[l(x + 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == playerId) { return true; }
+            if (Game.gameArray[x, l(y + 3)] != Constants.WALL_ID && Game.gameArray[x, l(y + 4)] == playerId) { return true; }
         }
-
-        // // if there is a player and no walls
-        // if (Game.gameArray[l(x - 2), y] == oppositePlayerId && 
-        // (Game.gameArray[l(x - 4), y] == playerId || Game.gameArray[l(x - 2), l(y - 2)] == playerId || Game.gameArray[l(x - 2), l(y + 2)] == playerId)) {
-        //     if (Game.gameArray[l(x - 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x - 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x - 3), y] != Constants.WALL_ID && Game.gameArray[l(x - 4), y] == Constants.FIELD_ID) { return true; }
-        // }
-
-        // if (Game.gameArray[l(x + 2), y] == oppositePlayerId && 
-        // (Game.gameArray[l(x + 4), y] == playerId || Game.gameArray[l(x + 2), l(y - 2)] == playerId || Game.gameArray[l(x + 2), l(y + 2)] == playerId)) {
-        //     if (Game.gameArray[l(x + 2), l(y - 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x + 2), l(y + 1)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x + 3), y] != Constants.WALL_ID && Game.gameArray[l(x + 4), y] == Constants.FIELD_ID) { return true; }
-        // }
-
-        // if (Game.gameArray[x, l(y - 2)] == oppositePlayerId &&
-        // (Game.gameArray[x, l(y - 4)] == playerId || Game.gameArray[l(x - 2), l(y - 2)] == playerId || Game.gameArray[l(x + 2), l(y - 2)] == playerId)) {
-        //     if (Game.gameArray[l(x - 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x + 1), l(y - 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y - 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[x, l(y - 3)] != Constants.WALL_ID && Game.gameArray[x, l(y - 4)] == Constants.FIELD_ID) { return true; }
-        // }
-
-        // if (Game.gameArray[x, l(y + 2)] == oppositePlayerId &&
-        // (Game.gameArray[x, l(y + 4)] == playerId || Game.gameArray[l(x - 2), l(y + 2)] == playerId || Game.gameArray[l(x + 2), l(y + 2)] == playerId)) {
-        //     if (Game.gameArray[l(x - 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x - 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[l(x + 1), l(y + 2)] != Constants.WALL_ID && Game.gameArray[l(x + 2), l(y + 2)] == Constants.FIELD_ID) { return true; }
-        //     if (Game.gameArray[x, l(y + 3)] != Constants.WALL_ID && Game.gameArray[x, l(y + 4)] == Constants.FIELD_ID) { return true; }
-        // }
 
         return false;
     }
