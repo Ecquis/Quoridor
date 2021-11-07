b083d163331b9966f65add87baac0acce3d3d688
Alexey Biriuchenko <gotastycake@gmail.com>
2021-11-07 12:32:13 +0200

    add init commit for bot

---
+++ bot/QuoridorBotGolf/Bot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuoridorBotGolf
+{
+    class Bot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
